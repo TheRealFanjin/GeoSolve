16525666eb1bd2c30c832e06b8ca68f8d29a98ae
fanjin <fanjin627@gmail.com>
2025-07-25 20:46:04 -0500

    update tensorflow

--- python-geosolve/main.py
+++ python-geosolve/main.py
@@ -4,94 +4,71 @@ from keras.layers import Dense
 from keras import metrics
 from keras import optimizers
 from keras import losses
+from keras import layers
 import zipfile as zp
 import numpy as np
 import pandas as pd
 import os
+import pathlib
 import io
 import matplotlib.pyplot as plt
 from keras.callbacks import ModelCheckpoint
 
 #config
-MAIN_ZIP = "dataset/archive.zip"
 INTERNAL_DATA = "dataset/"
-CSV_PATH = INTERNAL_DATA + "coords.csv"
 IMG_SIZE = (512, 512)
 BATCH_SIZE = 32
 EPOCHS = 50
-BUFFER_SIZE = 1024
+SHUFFLE_BUFFER_SIZE = 1000
+AUTOTUNE = tf.data.AUTOTUNE
 
 MODEL_SAVE_PATH = "saved_models"
 MODEL_FILENAME_FORMAT = os.path.join(MODEL_SAVE_PATH, "model_epoch_{epoch:02d}.keras")
 
-with zp.ZipFile(MAIN_ZIP,'r') as zf:
-    with zf.open(CSV_PATH) as csvBytes:
-        data = io.StringIO(csvBytes.read().decode("utf-8"))
-        setOfCoords = pd.read_csv(data, header=None)
-        setOfCoords.columns = ["longitude","latitude"]
 
-imageLabelMap = {}
-for index, row in setOfCoords.iterrows():
-    key = INTERNAL_DATA + f"{index}.png"
-    value = np.array([row["longitude"], row["latitude"]], dtype = np.float32)
-    imageLabelMap[key] = value
+print('loading dataset')
+countries = np.array(sorted([item.name for item in pathlib.Path(os.getcwd() + '\\dataset\\train').glob('*')]))
 
-def img_data_generator():
-    with zp.ZipFile(MAIN_ZIP, 'r') as zf:
-        for imgFileName, coords in imageLabelMap.items():
-            with zf.open(imgFileName) as imgFile:
-                imgBytes = imgFile.read()
-                yield imgBytes, coords
 
-def preprocess_img(imgBytes, coords):
-    img = tf.image.decode_image(imgBytes,channels=3, expand_animations=False)
-    img.set_shape([None, None, 3])
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    img = tf.image.resize(img, IMG_SIZE)
-    return img, coords
+def preprocess(path):
+    # get encoded country label
+    country = tf.argmax(tf.strings.split(path, os.path.sep)[-3] == countries)
 
-def preprocess_img_training(imgBytes, coords):
-    img, coords = preprocess_img(imgBytes,coords)
-    #Data augmentation
-    img = tf.image.random_flip_left_right(img)
-    img = tf.image.random_brightness(img, max_delta=0.2)
-    img = tf.image.random_contrast(img, lower=0.8, upper=1.2)
-    return img, coords
+    # decode img
+    image_bytes = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image_bytes, channels=3)
+    return image, country
 
-def training_dataset_pipeline():
-    dataset = tf.data.Dataset.from_generator(
-        lambda: img_data_generator(),
-        output_signature=(
-            tf.TensorSpec(shape=(), dtype=tf.string),
-            tf.TensorSpec(shape=(2,), dtype=tf.float32)
-        )
-    )
-    dataset = dataset.map(preprocess_img_training, num_parallel_calls=tf.data.AUTOTUNE)
-    dataset = dataset.shuffle(buffer_size=BUFFER_SIZE)
-    dataset = dataset.batch(batch_size=BATCH_SIZE)
-    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
-    return dataset
+# Load dataset
+train_ds = tf.data.Dataset.list_files('dataset/train/*/*/*.jpeg', shuffle=False)
+train_ds = train_ds.map(preprocess, num_parallel_calls=AUTOTUNE)
+for image, label in train_ds.take(1):
+    print(image.shape)
+train_ds = train_ds.shuffle(SHUFFLE_BUFFER_SIZE)
+train_ds = train_ds.batch(BATCH_SIZE).prefetch(AUTOTUNE)
 
-trainDataset = training_dataset_pipeline()
 
+print('loading model')
 model = keras.Sequential(layers=[
-    keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same", input_shape=IMG_SIZE + (3, )),
-    keras.layers.Conv2D(32,(3, 3), activation="relu", padding="same"),
+    keras.Input(shape=IMG_SIZE + (3,)),
+    keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same"),
+    keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same"),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64,(3, 3), activation="relu", padding="same"),
+    keras.layers.Conv2D(256, (3, 3), activation="relu", padding="same"),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation="relu"),
-    keras.layers.Dense(2, activation="linear")
+    keras.layers.GlobalAvgPool2D(),
+    keras.layers.Dense(512, activation="relu"),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(94, activation="softmax")
 ])
 model.compile(
     optimizer=optimizers.Adam(learning_rate=1e-3),
-    loss=losses.MeanSquaredError(),
-    metrics=[
-        metrics.MeanSquaredError()
-    ]
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
 )
+
+model.summary()
 
 checkpoint_callback_n_steps = ModelCheckpoint(
      filepath=os.path.join(MODEL_SAVE_PATH, "model_batch_{batch:05d}.keras"),
@@ -99,16 +76,16 @@ checkpoint_callback_n_steps = ModelCheckpoint(
      save_weights_only=False,
      verbose=1
  )
-
+print('training')
 history = model.fit(
-    trainDataset,
+    train_ds,
     epochs=EPOCHS,
     callbacks=[checkpoint_callback_n_steps]
     )
 
 FINAL_MODEL_PATH = os.path.join(MODEL_SAVE_PATH, "final_trained_model")
 os.makedirs(FINAL_MODEL_PATH, exist_ok=True)
-model.save(FINAL_MODEL_PATH)
+model.save(FINAL_MODEL_PATH + '\\model.keras')
 
 loss = history.history["loss"]
 epochsRange = range(1, EPOCHS + 1)
